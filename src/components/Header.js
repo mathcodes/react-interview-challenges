d900569f3e61747634fdbb99c0fca614ec49681c
mathcodes <jonpchristie@gmail.com>
2023-02-27 08:22:41 -0500

    8b

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ function Header({ fName, id, handleSignIn, handleSignOut, user }) {
       <div className="inline-flex">
         <h1
           className="text-2xl font-bold text-white"
-        >Chat App</h1>
+        >Backed</h1>
 <div>{fName}</div>
       </div>
       <div>
@@ -39,7 +39,7 @@ function Header({ fName, id, handleSignIn, handleSignOut, user }) {
           onClick={handleSignOut}>Logout</button>
         ) : (
           <button
-          className="text-orange-300 border rounded-md text-md bg-zinc-800 border-zinc-400 hover:border-orange-700"
+          className="p-2 m-1 text-orange-300 border rounded-md text-md bg-zinc-800 border-zinc-400 hover:border-orange-700"
           onClick={handleSignIn}>Login</button>
         )}
       </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -7,10 +7,41 @@ export default function Main() {
   return (
 <div class="text-white container">
 
-    <h1 class="text-4xl font-bold text-white text-center pt-10">Welcome!</h1>
+    <h1 class="text-4xl font-bold text-white text-center pt-10">Welcome to the  </h1>
+      <h3 class=" text-4xl font-bold text-white text-center pt-10">
+        <span className="text-white hover:text-orange-500">
+          R</span>
+        <span className="text-white hover:text-red-500">
+          e</span>
+        <span className="text-white hover:text-teal-500">
+          c</span>
+        <span className="text-white hover:text-green-500">
+          i</span>
+        <span className="text-white hover:text-yellow-500">
+          p</span>
+        <span className="text-white hover:text-green-700">
+          e</span>
+        <span className="text-white hover:text-blue-700">
+        </span>
+        <span className="text-white hover:text-purple-300">
+          G</span>
+        <span className="text-white hover:text-pink-700">
+          r</span>
+          <span className="text-white hover:text-teal-500">
+          a</span>
+        <span className="text-white hover:text-orange-500">
+          b</span>
+          <span className="text-white hover:text-green-500">
+          b</span>
+        <span className="text-white hover:text-orange-500">
+          e</span>
+        <span className="text-white hover:text-orange-500">
+          r</span>
+
+
+        ecipe Grabber </h3>
     <div class="flex flex-row justify-center">
-      <h3 class="flex text-2xl font-bold text-white text-center pt-10">Recipe Grabber - </h3>
-      <p><GiMeal />
+      <p>
       This app utilizes Tailwind CSS, React, Firebase, and some 3rd-party APIs to create a recipe search engine. Users can search for recipes by ingredient, and save recipes to their
       </p>
    </div>
