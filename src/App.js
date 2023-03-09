96c2fbd7f38b3436f92f33deff25f385a11f0fd2
mathcodes <jonpchristie@gmail.com>
2023-03-08 21:39:53 -0500

    working with ui rendering ingput

--- src/App.js
+++ src/App.js
@@ -1,59 +1,65 @@
-import React, { useState, useContext } from 'react';
-import { firebase } from './firebase/firebase';
-import { FirebaseContext } from './firebase/index';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import Chat from './components/Chat';
-import Weather from './components/Weather';
-import RecipeGrabber from './components/RecipeGrabber';
-import Header from './components/Header';
-import UserProfile from './components/UserProfile';
-import LoginPage from './components/LoginPage';
-import './App.css';
-
+import React, { useState } from 'react';
 
 function App() {
-  const [user, setUser] = useState(null);
-  const [fName, setFName] = useState(null);
-  const firebaseInstance = useContext(FirebaseContext);
-
-  // Function to handle signing in with Google account
-  const handleSignIn = () => {
-    const provider = new firebaseInstance.auth.GoogleAuthProvider();
-    firebaseInstance.auth().signInWithPopup(provider);
-  };
-
-  // Function to handle signing out
-  const handleSignOut = () => {
-    firebase.auth().signOut();
-  };
-
-  // Listen for changes in user authentication state
-  firebase.auth().onAuthStateChanged((user) => {
-    setUser(user);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: ''
   });
 
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  }
 
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    document.getElementById('text').innerHTML = `
+      <h1>Thank you for registering, ${formData.name}!</h1>
+      <p>You will receive an email at ${formData.email}.</p>
+    `;
+  }
 
   return (
-    <Router>
-      <div
-          className="container flex flex-col min-h-screen mx-auto bg-gradient-to-t from-zinc-900 to-zinc-300"
-      >
-        <Header user={user} fName={fName} handleSignIn={handleSignIn} handleSignOut={handleSignOut} id={user ? user.uid : null} />
-        <Routes>
-          <Route path="/" element={user ? <Main /> : <LoginPage />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/recipes" element={<RecipeGrabber />} />
-          <Route path="/weather" element={<Weather />} />
-          <Route path="/profile" element={user ? <UserProfile  user={user} handleLogout={handleSignOut} /> : <LoginPage />} />
-        </Routes>
-      </div>
-        <Footer />
-{/* wrap fotter in div that makes it always on botto fo page */}
-    </Router>
+    <>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+    <div id="text"></div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import FirebaseProvider from './firebase';
 
-ReactDOM.render(
-  <FirebaseProvider>
-    <App />
-  </FirebaseProvider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
